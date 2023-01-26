d73eb8ba8f083066fd65e165a43fef59adec2c13
adisetiawan1883 <123556193+adisetiawan1883@users.noreply.github.com>
2023-01-26 09:53:36 +0800

    Add files via upload

---
+++ page/tentang.php
@@ -0,0 +1,13 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil</b></p>
+</div>
+<style>
+
+</style>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <p>toko online sepatu terpercaya dan termurah di Indonesia yang menjual sepatu beragam jenis dan brand ke seluruh masyarakat Indonesia. Kami jual sepatu futsal siapapun untuk berbelanja secara online di website ini dengan cara dan pembayaran yang mudah dan dapat dilakukan kapanpun dan dimanapun.</p>
+  
+
+</div>
